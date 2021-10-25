fea2f4765f151e1ae25200e22780b6c18338161b
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2021-10-25 23:13:58 +0300

    fix non correct url in swager docs

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,20 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  const config = new DocumentBuilder()
-    .setTitle('Metamorphosa')
-    .setDescription('This REST API docs Metamorphosa')
-    .setVersion('1.0.0')
-    .build()
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
 
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('/api/docs', app, document)
-  
-  app.setGlobalPrefix('api')
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Metamorphosa')
+      .setDescription('This REST API docs Metamorphosa')
+      .setVersion('1.0.0')
+      .build(),
+  );
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(7000);
 }
+
 bootstrap();
