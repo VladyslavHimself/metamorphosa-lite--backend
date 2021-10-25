3e63417731bf73b977096a31332d348e547c0e87
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2021-10-25 20:22:34 +0300

    edit main.ts, connect swager. add global url prefix "api" add package: "swagger-ui-express"

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder()
+    .setTitle('Metamorphosa')
+    .setDescription('This REST API docs Metamorphosa')
+    .setVersion('1.0.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/api/docs', app, document)
+  
+  app.setGlobalPrefix('api')
   await app.listen(7000);
 }
 bootstrap();
