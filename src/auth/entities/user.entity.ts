bc023834f968ec6aac6993212bac0dbc1712d2ed
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2021-11-03 04:41:27 +0200

    add tags for API docs

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,7 +1,10 @@
+import { ExcerciseEntity } from 'src/excercise/entities/excercise.entity';
+import { TrainingEntity } from 'src/training/training.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -18,4 +21,13 @@ export class UserEntity {
 
   @CreateDateColumn()
   isCreate: Date;
+
+  @OneToMany(() => TrainingEntity, training => training.user)
+  trainings: TrainingEntity[]
+
+  
+  @OneToMany(() => ExcerciseEntity, excercise => excercise.user)
+  excercises: ExcerciseEntity[]
+
+
 }
