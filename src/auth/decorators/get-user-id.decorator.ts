541e9047e72aa610049ba9f314a4ceec1bac7da0
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2021-11-19 08:46:40 +0200

    delete auth module

--- src/auth/decorators/get-user-id.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-('@nestjs/common');
-
-export const GetUserId = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user;
-  },
-);
